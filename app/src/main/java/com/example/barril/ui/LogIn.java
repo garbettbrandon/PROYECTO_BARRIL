272930c01e5cde7c7aadb963ba02cfac4137ee22
robertdenism <robert1994denis@gmail.com>
2023-01-12 16:50:07 +0100

    3er

--- app/src/main/java/com/example/barril/ui/LogIn.java
+++ app/src/main/java/com/example/barril/ui/LogIn.java
@@ -1,14 +1,16 @@
-package com.example.barril;
+package com.example.barril.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.barril.R;
+
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
     }
 }
