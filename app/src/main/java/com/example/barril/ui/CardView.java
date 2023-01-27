3c4acc0bbaae90598f178a46c7748888cd2de322
robertdenism <robert1994denis@gmail.com>
2023-01-27 21:17:56 +0100

    cardview a medias

---
+++ app/src/main/java/com/example/barril/ui/CardView.java
@@ -0,0 +1,16 @@
+package com.example.barril.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.barril.R;
+
+public class CardView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
