329497f0a9763082cd4309616a00378ac133ba05
robertdenism <robert1994denis@gmail.com>
2023-01-30 17:59:12 +0100

    cardview a medias3

--- app/src/main/java/com/example/barril/ui/CardView.java
+++ app/src/main/java/com/example/barril/ui/CardView.java
@@ -3,14 +3,22 @@ package com.example.barril.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.barril.R;
 
 public class CardView extends AppCompatActivity {
 
+    ImageView botellaCerveza;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+
+        botellaCerveza = findViewById(R.id.botellaCerveza);
+
+        botellaCerveza.bringToFront();
+        botellaCerveza.setElevation(50);
     }
 }
