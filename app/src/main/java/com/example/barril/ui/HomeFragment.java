b0e54936ce7a8f6dc5c79db3c4f39d4ba1c62e68
robertdenism <robert1994denis@gmail.com>
2023-02-22 18:20:19 +0100

    introducidas cervezas

--- app/src/main/java/com/example/barril/ui/HomeFragment.java
+++ app/src/main/java/com/example/barril/ui/HomeFragment.java
@@ -33,10 +33,10 @@ public class HomeFragment extends Fragment {
         elements = new ArrayList<>();
         //elements.add(new ListElement(R.drawable.botella_mahou,R.drawable.logo_mahou,"mahou","una cerveza muy buena","3","3","3","#FFFFFf"));
         //elements.add(new ListElement(R.drawable.botella_cibeles,R.drawable.logo_cibeles, "Cibeles", "otra cerve","5","6","5","#f4f4f4"));
-        elements.add(new ListElement(R.drawable.goose_botella,R.drawable.logo_cibeles, "Goose", "otra cerve","2,82€","0,33cl","5,8%","#C68B3D"));
-        elements.add(new ListElement(R.drawable.abadia_botella,R.drawable.logo_cibeles, "Abadia", "otra cerve","2,49€","0,33cl","5,0%","#ED999D"));
-        elements.add(new ListElement(R.drawable.birraandblues_botella,R.drawable.logo_cibeles, "Birra & Blues", "otra cerve","2,58€","0,33cl","7,5%","#77BC71"));
-        elements.add(new ListElement(R.drawable.la_virgen_botella,R.drawable.la_virgen_logo, "La Virgen", "otra cerve","2,20€","0,33cl","5,2%","#F83B36"));
+        elements.add(new ListElement(R.drawable.goose_botella,R.drawable.goose_logo, "Goose", "Goose Blates es una cerveza intensa y de carácter robusto. Es amarga y con mucho cuerpo, por eso es una Winter Ale rojiza y de persistente espuma.","2,82€","0,33cl","5,8%","#C68B3D"));
+        elements.add(new ListElement(R.drawable.abadia_botella,R.drawable.cervezas_abadia_logo, "Abadia", "La Cerveza Artesana Abadía Española está hecha con mimo, con materias primas seleccionadas por expertos malteros a partir de cebada de primavera procedentes de territorio español.","2,49€","0,33cl","5,0%","#ED999D"));
+        elements.add(new ListElement(R.drawable.birraandblues_botella,R.drawable.birrablues_logo, "Birra & Blues", "Una doble Red Ale de estilo británico elaborada con cuatro tipos de malta y tres lúpulos empleando en uno de ellos la técnica de dry-hopping.","2,50€","0,33cl","6,4%","#77BC71"));
+        elements.add(new ListElement(R.drawable.la_virgen_botella,R.drawable.la_virgen_logo, "La Virgen", "Una cervecera con carácter madrileño. Para elaborar semejantes cervezas mucho tiene que ver la fábrica, la cervecería, el corazón de Cervezas La Virgen.","2,29€","0,33cl","6,5%","#F83B36"));
         elements.add(new ListElement(R.drawable.paquita_brown_botella,R.drawable.paquita_brown_logo, "Paquita Brown", "Curioso nombre para una cerveza especial.","2,49€","0,33cl","5,2%","#845D49"));
         elements.add(new ListElement(R.drawable.kadabra_botella,R.drawable.kadabra, "Kadabra", "La cerveza artesana Golden Ale de Kadabra es una cerveza rubia, dorada y de complejo carácter.","2,49€","0,33cl","4,8%","#0083B4"));
         elements.add(new ListElement(R.drawable.la_socarrada_botella,R.drawable.la_socarrada_logo, "La Socarrada", "Es una cerveza artesana premiun de estilo Winter Honey Ale, elaborada con Romero y Miel de Romero.","2,59€","0,33cl","6,0%","#7D7651"));
